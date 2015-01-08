3b09fc11fda3c87ba3d2c4248f0352e5f3e9d73e
Cal Stephens <calstephens98@gmail.com>
2015-01-07 22:22:42 -0500

    Submenus working in isolation

--- Inflation Calc WatchKit Extension/ValueController.swift
+++ Inflation Calc WatchKit Extension/ValueController.swift
@@ -9,15 +9,46 @@
 import Foundation
 import WatchKit
 
+enum DecimalState {
+    case Disabled, ReadyOne, ReadyTwo, Full
+}
+
 class ValueController : WKInterfaceController {
     
-    var currentValue = 0
+    var currentValue : Double = 0
+    var decimalState : DecimalState = .Disabled
     @IBOutlet weak var label: WKInterfaceLabel!
     
     func numberButtonPressed(value: Int){
-        currentValue *= 10
-        currentValue += value
-        label.setText("$\(currentValue)")
+        if decimalState == .ReadyOne {
+            currentValue += Double(value) / 10
+            decimalState = .ReadyTwo
+        }
+        else if decimalState == .ReadyTwo {
+            currentValue += Double(value) / 100
+            decimalState = .Full
+        }
+        else if decimalState == .Disabled{
+            currentValue *= 10
+            currentValue += Double(value)
+        }
+        updateLabel()
+    }
+    
+    @IBAction func decimalButton() {
+        decimalState = .ReadyOne
+        updateLabel()
+    }
+    
+    @IBAction func clearButton() {
+        decimalState = .Disabled
+        currentValue = 0
+        updateLabel()
+    }
+    
+    
+    func updateLabel(){
+        label.setText(formatAmount(currentValue, decimalState != .Disabled))
     }
     
     @IBAction func button1() {
@@ -59,4 +90,35 @@ class ValueController : WKInterfaceController {
     @IBAction func button0() {
         numberButtonPressed(0)
     }
+    
+}
+
+func formatAmount(amount:Double, hasDecimal:Bool) -> String {
+    var floatRounded = String(NSString(format: "%.02f", amount))
+    
+    let range = Range<String.Index>(start: floatRounded.startIndex, end: floatRounded.endIndex.predecessor().predecessor().predecessor())
+    var withoutDecimal = floatRounded.substringWithRange(range)
+    
+    
+    var pieces : Array<String> = []
+    while(countElements(withoutDecimal) > 3){
+        var index = withoutDecimal.endIndex.predecessor().predecessor().predecessor()
+        pieces.append(withoutDecimal.substringFromIndex(index))
+        withoutDecimal = withoutDecimal.substringToIndex(index)
+    }
+    pieces.append(withoutDecimal)
+    
+    pieces = pieces.reverse()
+    var moneyFormatted = pieces[0]
+    if(pieces.count > 1){
+        for i in 1...(pieces.count - 1){
+            moneyFormatted += ",\(pieces[i])"
+        }
+    }
+    
+    if(hasDecimal){
+        moneyFormatted += floatRounded.substringFromIndex(range.endIndex)
+    }
+    
+    return "$\(moneyFormatted)"
 }
--- Inflation Calc WatchKit Extension/YearController.swift
+++ Inflation Calc WatchKit Extension/YearController.swift
@@ -11,6 +11,49 @@ import WatchKit
 
 class YearController : WKInterfaceController {
     
+    var year : String = "1980"
     
+    @IBOutlet weak var valueLabel: WKInterfaceButton!
+    @IBOutlet weak var centuryLabel: WKInterfaceLabel!
+    @IBOutlet weak var decadeLabel: WKInterfaceLabel!
+    @IBOutlet weak var yearLabel: WKInterfaceLabel!
+    
+    @IBOutlet weak var centurySlider: WKInterfaceSlider!
+    @IBOutlet weak var decadeSlider: WKInterfaceSlider!
+    @IBOutlet weak var yearSlider: WKInterfaceSlider!
+    
+    @IBAction func centuryUpdated(float: Float) {
+        let value = Int(float)
+        year = "\(value)" + year.substringWithRange(Range<String.Index>(start: advance(year.startIndex, 2), end: year.endIndex))
+        centuryLabel.setText("\(value)")
+        updateLabel()
+    }
+    
+    @IBAction func decadeUpdated(float: Float) {
+        let value = Int(float)
+        year = year.substringWithRange(Range<String.Index>(start: year.startIndex, end: advance(year.endIndex, -2))) + "\(value)" + year.substringWithRange(Range<String.Index>(start: advance(year.startIndex, 3), end: year.endIndex))
+        decadeLabel.setText("\(value)")
+        updateLabel()
+    }
+    
+    @IBAction func yearUpdated(float: Float) {
+        let value = Int(float)
+        year = year.substringWithRange(Range<String.Index>(start: year.startIndex, end: advance(year.endIndex, -1))) + "\(value)"
+        yearLabel.setText("\(value)")
+        updateLabel()
+    }
+    
+    func updateLabel() {
+        valueLabel.setTitle(year)
+        
+        if year.toInt()! > 2015 {
+            year = "2015"
+            decadeLabel.setText("1")
+            yearLabel.setText("5")
+            decadeSlider.setValue(1)
+            yearSlider.setValue(5)
+            updateLabel()
+        }
+    }
     
 }
